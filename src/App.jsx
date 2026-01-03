93e2bea4a64fe7b69983fd4ec2132e0627a9ec72
Rakib Boshunia <fireaiboshunia.info@gmail.com>
2026-01-03 17:41:00 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './components/Layout/AppLayout'
+
+import Home from "./pages/Home"
+import About from './pages/About'
+import Country from './pages/Country'
+import Contact from './pages/Contact'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'Country',
+        element: <Country />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
